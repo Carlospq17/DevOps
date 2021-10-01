fd4d71137ab2810864be0f8e02680625d9562a71
DiegoAvmor <diegoavmor@gmail.com>
2021-10-01 14:15:16 -0500

    🧐 Updated attributes in user model,migration and seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable implements JWTSubject
      * @var string[]
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
@@ -30,18 +29,8 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-    
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,25 +24,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => Hash::make('root'), // password
-            'remember_token' => Str::random(10),
+            'status' => true,
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
 }
