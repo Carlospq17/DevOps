9703b24f51ef43af188736ec3fe07b6048fbd5ed
DiegoAvmor <diegoavmor@gmail.com>
2021-10-09 16:39:32 -0500

    Added tests endpoints for the entity Sale

--- tests/Unit/SaleTest.php
+++ tests/Unit/SaleTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit;
 
 use Mockery;
 use Tests\TestCase;
+use App\Models\Sale;
+use App\Models\Client;
+use RefreshDatabase;
 
 class SaleTest extends TestCase
 {
@@ -13,26 +16,65 @@ class SaleTest extends TestCase
     {
         parent::setUp();
         $this->withoutMiddleware();
-        $this->saleMock = Mockery::mock('overload:App\Models\Sale');
+        $this->saleMock = Mockery::mock('overload:ClientRepository');
     }
 
-    public function test_get_all_clientes()
+    public function test_find_sale()
     {
-        $this->saleMock->shouldReceive('all')->once();
+        $this->saleMock->shouldReceive('findById')->once();
 
-        $this->call('GET', route('sale.all'));
+        $sale = Sale::all()->last();
+    
+        $this->call('GET', 'api/v1/sales/'.''. $sale->id)->assertStatus(200);
+    }
+
+    public function test_get_all_sales()
+    {
+        $this->saleMock->shouldReceive('findAll')->once();
+
+        $this->call('GET', route('sale.all'))->assertStatus(200);
     }
 
     public function test_sale_creation()
     {
-        $this->saleMock->shouldReceive('save')->once();
+        $this->saleMock->shouldReceive('createSale')
+        ->once()
+        ->andReturn(Sale::class);
+
+        $client = Client::all()->last();
 
         $parameters = [
-            'clientId' => 1,
+            'clientId' => $client->id,
             'total_amount' => 1234,
         ];
 
         $this->call('POST', route('sale.create', $parameters));
+    }
+
+    public function test_update_sale()
+    {
+        $this->saleMock->shouldReceive('updateSale')
+        ->once()
+        ->andReturn(Sale::class);
+
+        $sale = Sale::all()->last();
+
+        $parameters = [
+            'id' => $sale->id ,
+            'total_amount' => 8888,
+        ];
+
+        $this->call('PUT', route('sale.update', $parameters));
+    }
+
+    public function test_delete_sale(){
+        $this->saleMock->shouldReceive('deleteSale')
+        ->with(Sale::class)
+        ->once()
+        ->andReturnNull();
+
+        $sale = Sale::all()->last();
+        $this->call('DELETE', route('sale.delete', ['id'=> $sale->id]))->assertStatus(204);
     }
 
     
