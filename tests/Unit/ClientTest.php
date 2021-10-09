c14ce2ab2300a69f29028b6fd8c80afd42ccad2b
DiegoAvmor <diegoavmor@gmail.com>
2021-10-09 16:15:24 -0500

    Added final endpoints to tests Client entity

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -6,8 +6,7 @@ use Mockery;
 use Tests\TestCase;
 use App\Models\Client;
 use WithoutMiddleware;
-use Illuminate\Support\Facades\Log;
-use Faker\Generator as Faker;
+
 class ClientTest extends TestCase
 {
     protected $clientMock;
@@ -16,18 +15,30 @@ class ClientTest extends TestCase
     {
         parent::setUp();
         $this->withoutMiddleware();
-        $this->clientMock = Mockery::mock('overload:App\Models\Client');
+        $this->clientMock = Mockery::mock('overload:ClientRepository');
+    }
+
+    public function test_find_client()
+    {
+        $this->clientMock->shouldReceive('findById')->once();
+
+        $client = Client::all()->last();
+
+        $this->call('GET', 'api/v1/clients/'.''.$client->id);
     }
 
     public function test_get_all_clientes()
     {
-        $this->clientMock->shouldReceive('all')->once();
+        $this->clientMock->shouldReceive('findAll')->once();
 
-        $this->call('GET', route('client.all'));
+        $this->call('GET', route('client.all'))->assertStatus(200);
     }
 
     public function test_client_creation(){
-        $this->clientMock->shouldReceive('save')->once();
+        $this->clientMock->shouldReceive('createClient')
+        ->once()
+        ->andReturn(Client::class);
+        
         $parameters = [
             'email' => 'newmail@hotmail.com',
             'password' => '1234',
@@ -37,7 +48,34 @@ class ClientTest extends TestCase
             'phone_number' => 999999999
         ];
 
-        $this->json('POST', route('client.create', $parameters));
+        $this->json('POST', route('client.create', $parameters))->assertStatus(201);
+    }
+
+    public function test_update_client_information(){
+        $this->clientMock->shouldReceive('updateClient')
+        ->once()
+        ->andReturn(Client::class);
+
+        $parameters = [
+            'name' => 'NewName',
+            'lastname' => 'newLastname',
+            'address' => 'newAddress',
+            'phone_number' => 999999999
+        ];
+
+        $client = Client::all()->last();
+
+        $this->json('PUT', 'api/v1/clients/'.''.$client->id ,$parameters)->assertStatus(200);
+    }
+
+    public function test_delete_client(){
+        $this->clientMock->shouldReceive('deleteClient')
+        ->with(Client::class)
+        ->once()
+        ->andReturnNull();
+
+        $client = Client::all()->last();
+        $this->call('DELETE', 'api/v1/clients/'.''.$client->id)->assertStatus(204);
     }
 
 }
