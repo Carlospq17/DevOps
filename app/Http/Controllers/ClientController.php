c69da0dfdb948c4a0c9016cfec33e5ace452a587
davidita118 <david.isaac.iker@hotmail.com>
2021-10-17 13:28:44 -0500

    Client logs added

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -22,19 +22,36 @@ class ClientController extends Controller
         $client = $this->repository->findById($id);
 
         if(!$client) {
-            Log::warning(trans('logger.entity_not_found',['entityName'=>'Client','entityId'=>$id]));
+            Log::warning(
+                trans(
+                    'logger.error.entity_not_found',
+                    ['entityName' => 'Client', 'entityId' => $id]
+                )
+            );
             return response()->json(["message" => "Entity not Found"], 404);
         }
 
         $client->user;
-        Log::debug("Client Found", ["client"=> $client]);
+        Log::debug(
+            trans(
+                'logger.http_method.getId', 
+                ['entityName' => 'Client']
+            ),
+            ['client' => $client]
+        );
 
         return response()->json($client, 200);
     }
 
     public function getClient() {
         $clients = $this->repository->findAll();
-        Log::debug("Found Client List", ["clients"=> json_encode($clients)]);
+        Log::debug(
+            trans(
+                'logger.http_method.get',
+                ['entityName' => 'Client']
+            ),
+            ["clients" => json_encode($clients)]
+        );
 
         return response()->json($clients, 200);
     }
@@ -48,25 +65,42 @@ class ClientController extends Controller
         ]);
 
         if($validator->fails()) {
-            Log::warning("Invalid Client Update Request Structure",['errors'=>json_encode($validator->errors())]);
+            Log::warning(
+                trans(
+                    'logger.validate.invalid_put',
+                    ['entityName', 'Client']
+                ),
+                ['errors' => json_encode($validator->erros())]
+            );
+
             return response()->json($validator->errors(), 400);
         }
 
         $client = $this->repository->updateClient($request,$id);
 
-
         if(!$client){
-            Log::warning(trans('logger.entity_not_found',['entityName'=>'Client','entityId'=>$id]));
+            Log::warning(
+                trans(
+                    'logger.error.entity_not_found',
+                    ['entityName' => 'Client', 'entityId' => $id]
+                )
+            );
+
             return response()->json(["message" => "Entity not Found"], 404);
         }
 
-        Log::debug("Client Updated", ["client"=> json_encode($client)]);
+        Log::debug(
+            trans(
+                'logger.http_method.put',
+                ['entityName' => 'Client']
+            ),
+            ['client' => json_encode($client)]
+        );
 
         return response()->json($client, 200);
     }
 
     public function postClient(Request $request) {
-
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string',
@@ -77,18 +111,37 @@ class ClientController extends Controller
         ]);
 
         if($validator->fails()) {
-            Log::warning("Invalid Client Post Request Structure",['errors'=>json_encode($validator->errors())]);
+            Log::warning(
+                trans(
+                    'logger.validate.invalid_post',
+                    ['entityName', 'Client']
+                ),
+                ['errors' => json_encode($validator->errors())]
+            );
+
             return response()->json($validator->errors(), 400);
         }
 
         $client = $this->repository->createClient($request);
 
         if(!$client) {
-            Log::warning("Client already exists, no action was perfomed.");
+            Log::warning(
+                trans(
+                    'logger.error.entry_duplicate',
+                    ['entityName' => 'Client', 'key' => 'email', 'value' => $request->email]
+                )
+            );
+
             return response()->json(["message" => "Unprocessable Entity"], 422);
         }
 
-        Log::debug("Client Created", ["client"=> json_encode($client)]);
+        Log::debug(
+            trans(
+                'logger.http_method.post',
+                ['entityName' => 'Client']
+            ),
+            ['client' => json_encode($client)]
+        );
 
         return response()->json($client, 201);;
     }
@@ -97,11 +150,24 @@ class ClientController extends Controller
         $client = Client::find($id);
 
         if(!$client) {
-            Log::warning(trans('logger.entity_not_found',['entityName'=>'Client','entityId'=>$id]));
+            Log::warning(
+                trans(
+                    'logger.error.entity_not_found',
+                    ['entityName' => 'Client', 'entityId' => $id]
+                )
+            );
+
             return response()->json(["message" => "Entity not Found"], 404);
         }
 
         $this->repository->deleteClient($client);
+        Log::debug(
+            trans(
+                'logger.http_method.delete',
+                ['entityName' => 'Client']
+            )
+        );
+
         return response()->json([], 204);
     }
 }
